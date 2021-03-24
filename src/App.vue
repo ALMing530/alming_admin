fe6987f071b583847458c3f39b4c9dde259626eb
ALMing530 <wuxianming3@gmail.com>
2021-03-24 13:27:18 +0800

    prepare for develop

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,8 @@
 <template>
   <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <el-button>Hello</el-button>
   </div>
-  <router-view/>
+  <!-- <router-view/> -->
 </template>
 
 <style>
